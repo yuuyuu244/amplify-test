abdcc1a2119b1c246bf0ee2f7dff5f3d77a36e98
Yuki Kikuya <yuki.kikuya@rivine.com>
2023-04-12 11:22:41 +0900

    [UPDATE] auth func of frontend env

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { useState } from 'react'
+//import ReactDOM from 'react-dom/client'
 
+import { Amplify } from 'aws-amplify'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+import awsmobile from './aws-exports'
+
+Amplify.configure(awsmobile)
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       TEST
+//     </div>
+//   )
+// }
 function App() {
-
   return (
-    <>
-      <h1>TEST</h1>
-    </>
+    <div className="App">
+      <h1>test</h1>
+      <Authenticator>
+       {({ signOut, user }) => (
+        <main>
+          <h1>こんにちは、 {user.username} さん</h1>
+          <button onClick={signOut}>サインアウト</button>
+        </main>
+      )}
+      </Authenticator>
+    </div>
   )
 }
-
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  define: {
+    "window.global": {},
+  },
   plugins: [react()],
 })
